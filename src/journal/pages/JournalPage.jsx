3692a394567e3523584a2b6ee3841869f11f27ea
steeltx <realsteel.gnu@gmail.com>
2024-03-12 23:41:09 -0600

    Creación de vista cuando no hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../view";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem laboriosam voluptatem corrupti est, molestiae, aliquam quos pariatur qui sequi veritatis voluptate delectus magnam, in ex enim maxime quis eveniet minima.</Typography>
+            {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem laboriosam voluptatem corrupti est, molestiae, aliquam quos pariatur qui sequi veritatis voluptate delectus magnam, in ex enim maxime quis eveniet minima.</Typography> */}
+            <NothingSelectedView />
         </JournalLayout>
     )
 }
