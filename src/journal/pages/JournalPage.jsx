996088a774d58087654da3f60c6b973b61e409b8
steeltx <realsteel.gnu@gmail.com>
2024-03-12 23:31:55 -0600

    Creación de Navbar, Sidebar en layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant="h1">JournalPage</Typography>
-        </>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem laboriosam voluptatem corrupti est, molestiae, aliquam quos pariatur qui sequi veritatis voluptate delectus magnam, in ex enim maxime quis eveniet minima.</Typography>
+        </JournalLayout>
     )
 }
