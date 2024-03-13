3810d42d26a291de31ec3dfbbb178786866232f9
steeltx <realsteel.gnu@gmail.com>
2024-03-13 00:43:45 -0600

    Se agrega vista de las notas y galeria de imagenes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,28 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../view";
+import { NoteView, NothingSelectedView } from "../view";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem laboriosam voluptatem corrupti est, molestiae, aliquam quos pariatur qui sequi veritatis voluptate delectus magnam, in ex enim maxime quis eveniet minima.</Typography> */}
             <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: "white",
+                    backgroundColor: "error.main",
+                    ":hover": { backgroundColor: "error.main", opacity: 0.9},
+                    position: "fixed",
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{fontSize: 30}}/>
+            </IconButton>
         </JournalLayout>
     )
 }
